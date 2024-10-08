c1df33d15a211452a6dfd1e92c272936809c1bf0
Vianna2lee <vianna.lee@laetottenham.ac.uk>
2024-10-08 16:16:08 +0000

    Pending changes exported from your codespace

--- Task1.py
+++ Task1.py
@@ -6,42 +6,60 @@
 
 
 ###########################################
-
-def weird_calculation():
-    # get the length and width of the first triangle from the user
-    opp1 = float(input("Enter your first triangle's opposite side length: "))
-    adj1 = float(input("Enter your first triangle's adjacent side length: "))
-
-    # work out the hyp
-    import math
+import math
+def weird_calculation(opp1,adj1,opp2,abj2):
     hyp1 = math.sqrt(opp1**2 + adj1**2)
-
-    # get the length and width of the second triangle from the user
-    opp2 = float(input("Enter your second triangle's opposite side length: "))
-    adj2 = float(input("Enter your second triangle's adjacent side length: "))
-
-    # work out the hyp
-    import math
     hyp2 = math.sqrt(opp2**2 + adj2**2)
-
-    # create a third triangle with the hyp1 as the opp and hyp2 as the adj
     opp3 = hyp1
     adj3 = hyp2
-    
-    import math
     hyp3 = math.sqrt(opp3**2 + adj3**2)
     return hyp3
 
-weird_answer = weird_calculation()
-print(weird_answer)
+while True:
+    try:
+        opp1 = float(input("Enter your first triangle's opposite side length: "))
+    except ValueError:
+        print("Not accepted, try again")
+    else:
+        break
+
+while True:
+    try:
+        adj1 = float(input("Enter your first triangle's adjacent side length: "))
+    except ValueError:
+        print("Not accepted, try again")
+    else:
+        break
+
+while True:
+    try:
+        opp2 = float(input("Enter your second triangle's opposite side length: "))
+    except ValueError:
+        print("Not accepted, try again")
+    else:
+        break
+
+while True:
+    try:
+        adj2 = float(input("Enter your second triangle's adjacent side length: "))
+    except ValueError:
+        print("Not accepted, try again")
+    else:
+        break
+        
+ 
+print(weird_calculation(opp1,adj1,opp1,adj2))
 
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+    # the input for the variable need to be a number, can't be empty and can't be negative
 # 2. Validate the user's input based on your preconditions.
+    #Done 
 # 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
-
+    #Use define function helps to shorten codes, therefore easiler to look for where the mistake is.
+    # We only need to write the define function once and when change the code, we only need to change it once.
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
 # 2. In your new file add functions for SOH CAH TOA. Also for the sine and cosine rule.
